0975085fc5f474b99e55f7b671ed2b849f5840fb
xyzyg <yuktigupta98@gmail.com>
2018-04-08 18:03:13 +0530

    made changes on welcome page

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return "Welcome to student records API!"
+	return "Welcome to student records API! created by Yukti"
 
 #the id in object is not a string and in students everything should be in string so we use bson in json_util 
 @app.route('/students/', methods=['GET', 'POST'])
